849c214ddfe568ff6afa2363855524cb437525ed
Kilian <kilian@kilianvalkhof.com>
2009-12-09 16:06:35 +0100

    fix array.prototype.sum

--- ico.js
+++ ico.js
@@ -17,7 +17,8 @@ var Ico = {
 /* Supporting methods to make dealing with arrays easier */
 /* Note that some of this work to reduce framework dependencies */
 Array.prototype.sum = function() {
-  for (var i = 0, sum = 0; i < this.length; sum += this[i++]) {return sum;}
+  for (var i = 0, sum = 0; i < this.length; sum += this[i++]) {};
+  return sum;
 };
 
 if (typeof Array.prototype.max == 'undefined') {
